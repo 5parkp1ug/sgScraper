db1bfe65f897d16c7804e5489cb5962dbafca8bd
Abhinav Jha <work@fstine.com>
2016-12-24 21:11:03 +0530

    PEP8 standard fixes Removed unused imports

--- sgMovies_muli_proc.py
+++ sgMovies_muli_proc.py
@@ -1,62 +1,57 @@
 import urllib
-import Queue
 import urlparse
-from threading import Thread
 from bs4 import BeautifulSoup
 from datetime import datetime
 from multiprocessing import Pool
 
-
 level1_urls = []
 level2_urls = []
+
 
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    FAIL = '\033[9s1m'
     ENDC = '\033[0m'
 
+
 def scrape_level1(url):
+    bs = BeautifulSoup(url.read(), "lxml")
+    level1_tags = bs.findAll("a", {"class": "button"})
 
-	bs = BeautifulSoup(url.read(),"lxml")
-	level1_tags = bs.findAll("a",{"class":"button"})
-
-	for tag in level1_tags:
-		level1_urls.append(tag["href"])
-		scrape_level2(tag["href"])
-
+    for tag in level1_tags:
+        level1_urls.append(tag["href"])
+        scrape_level2(tag["href"])
 
 
 def scrape_level2(url):
+    # find the sub-categorization in the page and get their url
+    html = urllib.urlopen(url)
+    bs = BeautifulSoup(html.read(), "lxml")
 
-	#find the sub-categorization in the page and get their url
-	html = urllib.urlopen(url)
-	bs = BeautifulSoup(html.read(),"lxml")
+    no_of_links = bs.findAll("tr", {"onclick": True})
+    for link in no_of_links:
+        path = link['onclick'].strip("window.location='/").strip("=';").strip("?sortby").replace(" ", "%20")
+        full_url = url + path
+        level2_urls.append(full_url)
 
-	no_of_links = bs.findAll("tr",{"onclick":True})
-	for link in no_of_links:
-		path = link['onclick'].strip("window.location='/").strip("=';").strip("?sortby").replace(" ","%20")
-		full_url = url+path
-		level2_urls.append(full_url)
 
 def scrape_level3(full_url):
+    # find the name of the movies from the sub-categpry page
+    html = urllib.urlopen(full_url)
+    bs = BeautifulSoup(html.read(), "lxml")
 
-	
-	#find the name of the movies from the sub-categpry page
-	html = urllib.urlopen(full_url)
-	bs = BeautifulSoup(html.read(),"lxml")
+    movies = bs.findAll("a", {"href": True})
+    o = urlparse.urlparse(full_url)
+    url = o.netloc
+    for movie in movies:
+        # print bcolors.HEADER + "Movie Name - %s"%(movie.getText()) # Name of the movie
 
-	movies = bs.findAll("a",{"href":True})
-	o = urlparse.urlparse(full_url)
-	url = o.netloc
-	for movie in movies:
-		#print bcolors.HEADER + "Movie Name - %s"%(movie.getText()) # Name of the movie
-
-		path = movie['href'].replace(" ","%20")
-		if path != '/':	
-			print bcolors.OKBLUE + "http://"+url+path
+        path = movie['href'].replace(" ", "%20")
+        if path != '/':
+            print bcolors.OKBLUE + "http://" + url + path
 
 
 # capture current time
@@ -68,8 +63,8 @@ print level1_urls
 print level2_urls
 
 p = Pool(processes=7)
-p.map(scrape_level3,level2_urls)
+p.map(scrape_level3, level2_urls)
 p.close()
 p.join()
 # print current time minus the start time
-print datetime.now()-startTime
+print datetime.now() - startTime
